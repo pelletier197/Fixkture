b971213d5bffa526f74d49510c4b3ba691a56bd7
sunny pelletier <sunny.pelletier@qohash.com>
2020-11-11 18:15:59 -0500

    it works

--- src/io/github/pelletier197/fixture/generator/api/GenerateFixtureAction.kt
+++ src/io/github/pelletier197/fixture/generator/api/GenerateFixtureAction.kt
@@ -1,10 +1,11 @@
-package io.github.pelletier197.api
+package io.github.pelletier197.fixture.generator.api
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class GenerateFixtureAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent?) {
+    override fun actionPerformed(p0: AnActionEvent) {
         TODO("Not yet implemented")
     }
 }
+
