43613722f63e601b24095f3b2eebdf1b731446da
sunny pelletier <sunny.pelletier@qohash.com>
2020-11-12 18:45:07 -0500

    base to send fixtures

--- src/main/kotlin/io/github/pelletier197/fixkture/api/GenerateFixtureAction.kt
+++ src/main/kotlin/io/github/pelletier197/fixkture/api/GenerateFixtureAction.kt
@@ -1,10 +1,16 @@
 package io.github.pelletier197.fixkture.api
 
+import com.intellij.ide.util.TreeClassChooserFactory
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class GenerateFixtureAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
+    override fun actionPerformed(event: AnActionEvent) {
+        val classChooserFactory = TreeClassChooserFactory.getInstance(event.project)
+        val classChooser = classChooserFactory.createAllProjectScopeChooser("Select a target class for you fixture")
+        classChooser.showDialog()
+        val selectedClass = classChooser.selected
         TODO("Not yet implemented")
     }
 }
+
